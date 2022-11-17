c0e8650e1936ffaf3376a2f0c00f5e1b1a581f5f
Mi <suhaimi55@outlook.com>
2022-11-17 12:10:44 +0800

    completed app

--- webapp/main.py
+++ webapp/main.py
@@ -13,12 +13,17 @@ def login():
     return '<form method="post"><input type="password" name="title" placeholder="Password"></input><button type="submit">Login</button>'
 
 
-def verify_pw(pw):
-
-    if len(pw) > 7:
-        return True
-    else:
+def verify_pw(pw):  # https://owasp.org/www-project-proactive-controls/v3/en/c6-digital-identity
+    with open('10-million-password-list-top-1000.txt') as f:
+        if pw in f.read():
+            print("bad password")
+            return False
+    if len(pw) < 10:  # no MFA so minimum 10
+        print("short password")
         return False
+    else:
+        print(pw, "OK")
+        return True
 
 
 if __name__ == "__main__":
